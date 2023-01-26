5d55fca811ade6f2a973bf12b119428fd9f55d11
th333boo <th333boo@bbiwebsec.com>
2023-01-27 00:13:33 +0100

    io port scanner

--- io333.py
+++ io333.py
@@ -1,12 +1,33 @@
 #!/usr/bin/env python3
 import threading
 import socket
-HOST,PORTS,PORTC = "127.0.0.1",3338,[1:65535]
+import time
+
+HOST,PORTS,PORTC = "127.0.0.1",3338,3339
+io = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 def SrvSocket():
-    io = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     io.bind((HOST,PORTS))
 
 def CltSocket():
-    io = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     io.connect((HOST,PORTC))
+
+print("START SCANNING PORT")
+def PortScan(PORT):
+    status = False
+    try:
+        CltSocket()
+        print(CltSocket)
+        status = True
+    except:
+        status = False
+    if status:
+        print("port {} is open".format(PORT))
+start_time = time.time()
+  
+for i in range(0, 100000):
+    thread = threading.Thread(target=PortScan, args=[i])
+    thread.start()
+
+end_time = time.time()
+print("To scan all ports it took {} seconds".format(end_time-start_time))
