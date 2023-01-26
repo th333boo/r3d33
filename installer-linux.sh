c8ce261d41c6be97d5c3ea768fa70d365260ec7a
th333boo <th333boo@bbiwebsec.com>
2023-01-26 17:17:30 +0100

    install script server

---
+++ installer-linux.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+HEADER="
+### ### ### ### ### ### ### ### ###
+###         th333boo            ###
+### telegram: @th333boo         ###
+### www: https://th333boo.com   ###
+### mail: contact@th333boo.com  ###
+### ### ### ### ### ### ### ### ###"
+declare -a USER=("th333boo")
+declare -a SRV_URL=("***.ngrok.io" "io.th333boo.com")
+declare -a PORT("80" "443" "3338")
+declare -a TOOLS("python3" "tor")
+
+## TOOL + USER INSTALL
+## WEBSERVER CONFIG
+## TOR PROXY INSTALL
+## TOR CONFIG
