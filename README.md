64c8c0aba5a044b46c3ae2452432e7a770fcf732
th333boo <th333boo@bbiwebsec.com>
2023-02-01 10:35:10 +0100

    update install + move feature to toolkit

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Simple Malware Development for educational purposes, decline any responsibility for misuses of this tools
 
-To install the service run the following command:
+To install the service run the following command to setup the server.
 
-> wget -O - https://raw.githubusercontent.com/th333boo/r3dth1rth33n/master/installer-linux.sh | sh
+> wget -O https://raw.githubusercontent.com/th333boo/r3dth1rth33n/master/installer && chmod +x installer.sh && ./installer.sh
--- installer.sh
+++ installer.sh
@@ -10,45 +10,52 @@ HEADER="
 
 PATH_INSTALL_FOLDER='/opt'
 declare -a USER=("noc" "th333boo")
-declare -a SRV_URL=("" "io.th333boo.com")
+declare -a SRV_URL=("io.th333boo.com")
 cat $SRV_URL
 declare -a PORT("80" "443" "3338")
 declare -a TOOLS("python3" "tor" "git" "pip3")
 declare -a PIP_TOOLS("ed25519" "python-decouple" "scrapy")
 
+printf '\033[32m\nUPDATE OK \033[0m\n'
+echo "==============================="
 ## UPDATER SYSTEM
 git clone https://github.com/th333boo/r3dth1rth33n.git $PATH_INSTALL_FOLDER/
 git pull $PATH_INSTALL_FOLDER/r3dth1rth33n/
-chmod +x $PATH_INSTALL_FOLDER/r3dth1rth33n/installer-linux.sh | sh
-
-printf '\033[32m\nUPDATE OK \033[0m\n'
-echo "==============================="
-
-## HEADER INTRO
-cat $HEADER
+chmod +x $PATH_INSTALL_FOLDER/r3dth1rth33n/installer.sh
+sh $PATH_INSTALL_FOLDER/
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
 
 printf '\033[32m\nBANNER \033[0m\n'
 echo "==============================="
+## HEADER INTRO
+cat $HEADER
 
+printf '\033[32m\nUSER CREATED \033[0m\n'
+echo "==============================="
 ## CREATE USER MACHINE
 for str in ${USER[@]};
 do useradd ${USER[@]} -m -s /bin/bash -d "/home/${$USER[$str]}/"
 
-printf '\033[32m\nUSER CREATED \033[0m\n'
+printf '\033[32m\nTOOLS INSTALL FINISHED \033[0m\n'
 echo "==============================="
-
 ## INSTALL TOOLS
 apt update
 for str in ${TOOLS[@]};do apt install $str -y ; done
 for str in ${PIP_TOOLS[@]};do apt install $str -y ; done
 pip3 install --pre scapy[complete]
 
-printf '\033[32m\nTOOLS INSTALL FINISHED \033[0m\n'
+printf '\033[32m\nWEBSERVER INSTALL FINISHED \033[0m\n'
 echo "==============================="
 ## WEBSERVER CONFIG
 
+printf '\033[32m\nTOR INSTALL FINISHED \033[0m\n'
+echo "==============================="
 ## TOR PROXY INSTALL
 
+printf '\033[32m\nTOR CONFIG FINISHED \033[0m\n'
+echo "==============================="
 ## TOR CONFIG
 
+printf '\033[32m\nSPAM SYSTEM INSTALL FINISHED \033[0m\n'
+echo "==============================="
 ## SPAM SYSTEM
