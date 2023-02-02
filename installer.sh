e20132078068c9cf2f6171eda38dabc728026a42
th333boo <th333boo@bbiwebsec.com>
2023-02-03 00:22:05 +0100

    add nginx webserver config

--- installer.sh
+++ installer.sh
@@ -11,9 +11,8 @@ HEADER="
 PATH_INSTALL_FOLDER='/opt'
 declare -a USER=("noc" "th333boo")
 declare -a SRV_URL=("io.th333boo.com")
-cat $SRV_URL
 declare -a PORT("80" "443" "3338")
-declare -a TOOLS("python3" "tor" "git" "pip3")
+declare -a TOOLS("python3" "tor" "git" "pip3" "nginx")
 declare -a PIP_TOOLS("ed25519" "python-decouple" "scrapy")
 
 printf '\033[32m\nUPDATE OK \033[0m\n'
@@ -47,7 +46,24 @@ pip3 install --pre scapy[complete]
 printf '\033[32m\nWEBSERVER INSTALL FINISHED \033[0m\n'
 echo "==============================="
 ## WEBSERVER CONFIG
+mkdir -p /var/log/nginx/$SRV_URL
+tee /etc/nginx/conf.d/$SRV_URL > /dev/null << EOF
+## HTTPS CONFIG
+server {
+        listen 443 ssl http2;
+        listen [::]:443 ssl http2;
+        server_name www.$SRV_URL $SRV_URL;
 
+        root /var/www/$SRV_URL/;
+        index index.html;
+
+        ssl_certificate /etc/ssl/certs/ssl-cert-snakeoil.pem;
+        ssl_certificate_key /etc/ssl/private/ssl-cert-snakeoil.key;
+
+        access_log /var/log/nginx/$SRV_URL/access.log;
+        error_log /var/log/nginx/$SRV_URL/error.log warn;
+}
+EOF
 printf '\033[32m\nTOR INSTALL FINISHED \033[0m\n'
 echo "==============================="
 ## TOR PROXY INSTALL
