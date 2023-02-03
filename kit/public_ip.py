9da39a83eb5f0289649fd132275cc87935a7a5a6
th333boo <th333boo@bbiwebsec.com>
2023-02-03 21:13:03 +0100

    draf tor implent

--- kit/public_ip.py
+++ kit/public_ip.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 import requests,socket
 
-print('### [ IP ]###')
-HOSTNAME=socket.gethostname()   
-LOCAL_IP=socket.gethostbyname(HOSTNAME)   
-PUBLIC_IP = requests.get('http://ipinfo.io/json').json()['ip']
-print("Your Computer HOSTNAME is:"+ HOSTNAME +"\n")   
-print("Your Computer LOCAL_IP Address is:"+ LOCAL_IP +"\n")     
-print("Your Computer PUBLIC IP Address is:"+ PUBLIC_IP +"\n")   
+def PUBLIC_IP():
+    print('### [ IP ]###')
+    HOSTNAME=socket.gethostname()   
+    LOCAL_IP=socket.gethostbyname(HOSTNAME)   
+    PUBLIC_IP = requests.get('http://ipinfo.io/json').json()['ip']
+    print("Your Computer HOSTNAME is:"+ HOSTNAME +"\n")   
+    print("Your Computer LOCAL_IP Address is:"+ LOCAL_IP +"\n")     
+    print("Your Computer PUBLIC IP Address is:"+ PUBLIC_IP +"\n")   
+PUBLIC_IP()
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
-from .io.webserver import TB_HTTPServer
-from .io.websocket import *
-from .io.database import *
-from .bot.telegrambot import *
-from .bot.spambot import *
+from kit.public_ip import PUBLIC_IP
+from io.webserver import TB_HTTPServer
+from tor.tor import Create_TUNTOR()
+# from tor.th333booTOR import 
+# from .io.websocket import *
+# from .io.database import *
+# from .bot.telegrambot import *
+# from .bot.spambot import *
 
+PUBLIC_IP()
 TB_HTTPServer()
-
+Create_TUNTOR()
 ######################################
 print('### [ DNS TAKEOVER ] ###')
 
