032a850dd1147177c396c6477b7ff3ee96237e30
th333boo <th333boo@bbiwebsec.com>
2023-02-03 04:21:30 +0100

    workspace in main file

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from .io.webserver import TB_HTTPServer
+from .io.websocket import *
+from .io.database import *
+from .bot.telegrambot import *
+from .bot.spambot import *
+
+TB_HTTPServer()
+
+######################################
+print('### [ DNS TAKEOVER ] ###')
+
+######################################
+print('### [ ALL NETWORK TAKEOVER ] ###')
