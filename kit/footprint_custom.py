e8d737975fd2c876af8026918d8d8d0c9b557941
th333boo <th333boo@bbiwebsec.com>
2023-02-08 00:07:18 +0100

    function footcleaner export to main

--- kit/footprint_custom.py
+++ kit/footprint_custom.py
@@ -2,26 +2,27 @@
 from decouple import config
 from scapy.all import *
 
-print('### [ FOOTPRINT CUSTOM ] ###')
-HW_SRC = "FF:FF:FF:FF:FF:FF"
-HW_DST = "FF:FF:FF:FF:FF:FF"
-IP_SRC = "8.2.246.1"
-IP_DST = "8.3.11.1"
-IP_GW = "192.168.1.1"
-IP_SRC = config('IP_SRC')
-IP_DST = config('IP_DST')
+class FootPrint ():
+    print('### [ FOOTPRINT CUSTOM ] ###')
+    def __self__(self):
+        self.HW_SRC = "FF:FF:FF:FF:FF:FF"
+        self.HW_DST = "FF:FF:FF:FF:FF:FF"
+        # self.IP_SRC = "8.2.246.1"
+        # self.IP_DST = "8.3.11.1"
+        self.IP_GW = "192.168.1.1"
+        self.IP_SRC = config('IP_SRC')
+        self.IP_DST = config('IP_DST')
 
-
-print('### [ FOOTPRINT CLEANER ] ###')
-print('# [ BEFORE ] #')
-FRAME_IP = IP()
-FRAME = Ether()
-
-FRAME.show()
-FRAME_IP.show()
-
-print('# [ AFTER ] #')
-FRAME = Ether(src=HW_SRC, dst=HW_DST)
-FRAME_IP = IP(src=IP_SRC, dst=IP_DST, ttl=(111), frag=0, tos=0x0)
-FRAME.show()
-FRAME_IP.show()
+    print('### [ FOOTPRINT CLEANER ] ###')
+    def FootPrintClean(self,HW_SRC,HW_DST,IP_DST,IP_GW,IP_SRC,Ether,IP,FRAME_IP,FRAME):
+        print('# [ BEFORE ] #')
+        FRAME_IP = IP()
+        FRAME = Ether()
+        FRAME.show()
+        FRAME_IP.show()
+        print('# [ AFTER ] #')
+        FRAME = Ether(src=HW_SRC, dst=HW_DST)
+        FRAME_IP = IP(src=IP_SRC, dst=IP_DST, ttl=(111), frag=0, tos=0x0)
+        FRAME.show()
+        FRAME_IP.show()
+FootPrint()
