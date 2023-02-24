2fb68a7962cdcae0951fef44cf9faa7322328823
th333boo <th333boo@th333boo>
2023-02-24 09:03:59 +0100

    random update

---
+++ io/webserver.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+from http.server import HTTPServer,BaseHTTPRequestHandler
+HOST,PORT = "127.0.0.1",3339
+FORMAT = "utf-8"
+PAYLOAD=''
+
+class TB_HTTPServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/static/index.html'
+        try:
+            file_to_open = open(self.path[1:]).read()
+            self.send_response(200)
+            self.send_header("Content-Type", "text/html")
+            self.end_headers()
+        except:
+            file_to_open = 'Not found'
+            self.send_response(404)
+        self.wfile.write(bytes(file_to_open,FORMAT))
+
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "text/html")
+        self.end_headers()
+try:
+    webserver = HTTPServer((HOST, PORT),TB_HTTPServer)
+    webserver.serve_forever()
+except Exception as e:
+    print(e)
+    webserver.socket.close()
+TB_HTTPServer()
