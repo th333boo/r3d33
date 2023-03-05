404564d178a68a8ce7466778fd6e898bff13f16b
th333boo <th333boo@bbiwebsec.com>
2023-03-05 20:33:33 +0100

    class footprint test validation

--- kit/footprint_custom.py
+++ kit/footprint_custom.py
@@ -4,28 +4,26 @@
 from decouple import config
 from scapy.all import *
 
-FRAME_IP = IP()
-FRAME = Ether()
-class FootPrint ():
+class TB_FootPrintCustom ():
     print('### [ FOOTPRINT CUSTOM ] ###')
-    def __init__(self,HW_SRC,HW_DST,IP_GW,IP_SRC,IP_DST):
+    def __init__(self,HW_SRC=str(config('HW_SRC')),HW_DST=str(config('HW_DST')),IP_SRC=str("8.2.246.1"),IP_DST=str("8.3.11.1")):
         self.HW_SRC = HW_SRC
         self.HW_DST = HW_DST
-        # self.IP_SRC = "8.2.246.1"
-        # self.IP_DST = "8.3.11.1"
+        self.IP_SRC = IP_SRC
         self.IP_SRC = IP_SRC
         self.IP_DST = IP_DST
-        self.IP_GW = IP_GW
+        self.IP_GW = ''
+    print('OK')
     print('### [ FOOTPRINT CLEANER ] ###')
-    def FootPrintClean(self,HW_SRC,HW_DST,IP_DST,IP_SRC,Ether,IP,FRAME_IP,FRAME):
+    def TB_FootPrintClean(self,Ether,IP,FRAME_IP,FRAME):
         print('# [ BEFORE ] #')
+        FRAME=Ether()
+        IP=IP()
         FRAME.show()
         FRAME_IP.show()
         print('# [ AFTER ] #')
         FRAME = Ether(src=HW_SRC, dst=HW_DST)
         FRAME_IP = IP(src=IP_SRC, dst=IP_DST, ttl=(111), frag=0, tos=0x0)
         FRAME.show()
-        FRAME_IP.show()
-
-CurrentFrame = __init__(config('HW_SRC'),config('HW_DST'),config('IP_SRC'),config('IP_DST'),config('IP_GW'),config('IP_SRC'),config('IP_SRC'))
-
+        FRAME_IP.show() 
+    print('OK')
