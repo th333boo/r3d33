bf909ebc270053ca7e8bde44189ba234feb0746d
th333boo <th333boo@bbiwebsec.com>
2023-03-06 10:47:11 +0100

    move from server to io

--- io/webserver.py
+++ io/webserver.py
@@ -13,27 +13,28 @@ class TB_SSLWraper():
     httpd.serve_forever()
 
 class TB_HTTPServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/':
-            self.path = '/static/index.html'
-        try:
-            file_to_open = open(self.path[1:]).read()
-            self.send_response(200)
-            self.send_header("Content-Type", "text/html")
-            self.end_headers()
-        except:
-            file_to_open = 'Not found'
-            self.send_response(404)
-        self.wfile.write(bytes(file_to_open,FORMAT))
-
-    def do_POST(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "text/html")
-        self.end_headers()
-try:
-    webserver = HTTPServer((HOST, PORT),TB_HTTPServer)
-    webserver.serve_forever()
-except Exception as e:
-    print(e)
-    webserver.socket.close()
 TB_HTTPServer()
+
+#     def do_GET(self):
+#         if self.path == '/':
+#             self.path = '/static/index.html'
+#         try:
+#             file_to_open = open(self.path[1:]).read()
+#             self.send_response(200)
+#             self.send_header("Content-Type", "text/html")
+#             self.end_headers()
+#         except:
+#             file_to_open = 'Not found'
+#             self.send_response(404)
+#         self.wfile.write(bytes(file_to_open,FORMAT))
+
+#     def do_POST(self):
+#         self.send_response(200)
+#         self.send_header("Content-Type", "text/html")
+#         self.end_headers()
+# try:
+#     webserver = HTTPServer((HOST, PORT),TB_HTTPServer)
+#     webserver.serve_forever()
+# except Exception as e:
+#     print(e)
+#     webserver.socket.close()
