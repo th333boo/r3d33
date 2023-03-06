336670ef7e2c6575042ebdad91d1e537e0fa173d
th333boo <th333boo@bbiwebsec.com>
2023-03-06 10:39:28 +0100

    class creation for traceroute

--- kit/traceroute_takeover.py
+++ kit/traceroute_takeover.py
@@ -3,9 +3,12 @@
 
 from decouple import config
 from scapy.all import *
+from kit.footprint_custom import TB_FootPrintCustom
+from kit.public_ip import TB_PUBLIC_IP
 
-print('### [ TRACEROUTE ] ###')
-ans,unans=traceroute(PUBLIC_IP,l4=UDP(sport=RandShort())/DNS(qd=DNSQR(qname="th333boo.com")))
-res,unans = sr(IP(dst=PUBLIC_IP", ttl=(5,10), flags="MF")/UDP(sport=RandShort( ), dport=53), timeout=125)
+class TB_TraceRoute():
+    print('### [ TRACEROUTE ] ###')
+    ans,unans=sr(TB_PUBLIC_IP,l4=UDP(sport=RandShort())/DNS(qd=DNSQR(qname="th333boo.com")))
+    res,unans = sr(IP(dst=TB_PUBLIC_IP, ttl=(5,10), flags="MF")/UDP(sport=RandShort( ), dport=53), timeout=125)
 
-print('### [ TRACEROUTE TAKEOVER ] ###')
+    print('### [ TRACEROUTE TAKEOVER ] ###')
