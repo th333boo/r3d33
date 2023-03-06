59e5bbb723e70a3d2703307493b18575ea9c7513
th333boo <th333boo@bbiwebsec.com>
2023-03-06 10:42:23 +0100

    draft mysql connector

--- io/database.py
+++ io/database.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 from decouple import config
 from sqlalchemy import create_engine
-
 engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+
+import MySQLdb
+db = MySQLdb.connect(host="localhost",user="bbi",passwd="borntobein",db="bbi")
+cur = db.cursor()
+cur.execute("SELECT * FROM bbi")
+for row in cur.fetchall():
+    print row[0]
+db.close()
