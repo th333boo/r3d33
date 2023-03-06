8a1d03fc62b7c89956926d1adc66ac62a8017dcc
th333boo <th333boo@bbiwebsec.com>
2023-03-06 10:45:16 +0100

    draft of th333boo socket

--- io/websocket.py
+++ io/websocket.py
@@ -2,9 +2,29 @@
 import asyncio
 import websockets
 
-async def hello():
-    async with websockets.connect("ws://localhost:8765") as websocket:
-        await websocket.send("Hello world!")
-        await websocket.recv()
+connected = set()
 
-asyncio.run(hello())
+async def handler(websocket,path):
+    connected.add(websocket)
+    while True:
+        try:
+            if path == '/':
+                # TODO async for headerlistenner in websocket:
+                async for message in websocket:
+                    for conn in connected:
+                        await websocket.send("Welcome to the half demon portal \n @th333boo")
+
+        except websockets.ConnectionClosedOK:
+            break
+
+async def th333booWebSoc():
+    async with websockets.serve(handler, "127.0.0.1", 8443):
+        await asyncio.Future()  # run forever
+
+if __name__ == "__main__":
+    asyncio.run(th333booWebSoc())
+
+# async def th333booWebSoc():
+#     async with websockets.connect("ws://localhost:8765") as websocket:
+#         await websocket.send("Hello world!")
+#         await websocket.recv()
