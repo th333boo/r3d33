b4be2604a88747f7eaef2a8b69438821c8069c92
th333boo <th333boo@bbiwebsec.com>
2023-03-06 15:55:17 +0100

    draft webserver ssl implant

--- io/webserver.py
+++ io/webserver.py
@@ -13,9 +13,19 @@ class TB_SSLWraper():
     httpd.serve_forever()
 
 class TB_HTTPServer(BaseHTTPRequestHandler):
-    pass
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/static/index.html'
+        try:
+            file_to_open = open(self.path[1:]).read()
+            self.send_response(200)
+            self.send_header("Content-Type", "text/html")
+            self.end_headers()
+        except:
+            file_to_open = 'Not found'
+            self.send_response(404)
+        self.wfile.write(bytes(file_to_open,FORMAT))
 TB_HTTPServer()
-
 #     def do_GET(self):
 #         if self.path == '/':
 #             self.path = '/static/index.html'
