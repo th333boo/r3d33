544e9e36cfd4edaa1c047607e4f9c7e11274738a
th333boo <th333boo@bbiwebsec.com>
2023-03-06 15:56:36 +0100

    comment on path + console welcome message

--- io/websocket.py
+++ io/websocket.py
@@ -8,21 +8,25 @@ async def handler(websocket,path):
     connected.add(websocket)
     while True:
         try:
-            if path == '/':
-                # TODO async for headerlistenner in websocket:
-                async for message in websocket:
-                    for conn in connected:
-                        await websocket.send("Welcome to the half demon portal \n @th333boo")
+            # if path == '/':
+            #     # TODO async for headerlistenner in websocket:
+            async for msg in websocket:
+                for conn in connected:
+                    if conn != websocket:
+                        await websocket.send("🏴‍☠️ Welcome @th333boo station\n⚪ Wh1t3h4t3 3th1c4l h4ck3r ⚫\n☠️ Malware researcher")
 
         except websockets.ConnectionClosedOK:
             break
 
 async def th333booWebSoc():
-    async with websockets.serve(handler, "127.0.0.1", 8443):
-        await asyncio.Future()  # run forever
+    async with websockets.serve(handler, "127.0.0.1", 3333):
+ #       await asyncio.get_event_loop().run_until_complete(th333booWebSoc)
+        await asyncio.Future()
 
 if __name__ == "__main__":
     asyncio.run(th333booWebSoc())
+    asyncio.Future()  # run forever
+    # asyncio.get_event_loop().run_forever()
 
 # async def th333booWebSoc():
 #     async with websockets.connect("ws://localhost:8765") as websocket:
